5f11563745f743878379dc85e3179c1dd793c601
QUAFFquaff <632633533@qq.com>
2018-06-28 00:27:01 +0800

    save pass

--- loginform.cpp
+++ loginform.cpp
@@ -12,7 +12,7 @@ LoginForm::LoginForm(QWidget *parent) :
     ui(new Ui::LoginForm)
 {
     ui->setupUi(this);
-
+    load();
     this->setProperty("canMove", true);
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowSystemMenuHint | Qt::WindowMinMaxButtonsHint);
     setAttribute(Qt::WA_TranslucentBackground);
@@ -20,13 +20,42 @@ LoginForm::LoginForm(QWidget *parent) :
     IconHelper::Instance()->setIcon(ui->btnMenu_Min, QChar(0xF068));
     IconHelper::Instance()->setIcon(ui->btnMenu_Close, QChar(0xF00d));
 
+    if(ui->checkBox->isChecked()){
+        qDebug()<<"checked";
+        QString username;
+        QString password;
+        bool is_un = readInit(QString("./user.ini"), "username", username);
+        bool is_pass = readInit(QString("./user.ini"), "password", password);
+        ui->lineEdit_un->setText(username);
+        ui->lineEdit_pass->setText(password);
+
+    }
+
     //关联登录按钮点击
     connect(ui->pushButton_login,SIGNAL(clicked()),this,SLOT(doLoginButClick()));
 }
 
+
 LoginForm::~LoginForm()
 {
     delete ui;
+}
+
+void LoginForm::load()
+{
+
+    QSettings settings;
+    settings.beginGroup("./checkBox");
+    ui->checkBox->setChecked(settings.value("check1", true).toBool());
+    settings.endGroup();
+}
+
+void LoginForm::save()
+{
+    QSettings settings;
+    settings.beginGroup("./checkBox");
+    settings.setValue("check1", ui->checkBox->isChecked());
+    settings.endGroup();
 }
 
 QWidget *LoginForm::getDragnWidget()
@@ -37,9 +66,10 @@ QWidget *LoginForm::getDragnWidget()
 void LoginForm::doLoginButClick()
 {
 //    this->hide();
+    rememberPass();
     Dialog *d = new Dialog;
     d->show();
-
+    save();
 }
 
 void LoginForm::on_btnMenu_Min_clicked()
@@ -50,4 +80,59 @@ void LoginForm::on_btnMenu_Min_clicked()
 void LoginForm::on_btnMenu_Close_clicked()
 {
     close();
+}
+
+
+
+bool LoginForm::writeInit(QString path, QString user_key, QString user_value)
+{
+    if(path.isEmpty() || user_key.isEmpty()){
+        return false;
+    }
+    else{
+        //创建文件
+        QSettings * config =  new QSettings(path,QSettings::IniFormat);
+
+        //写入
+        config->beginGroup("config");
+        config->setValue(user_key,user_value);
+        config->endGroup();
+        return true;
+    }
+}
+
+bool LoginForm::readInit(QString path, QString user_key, QString &user_value)
+{
+    user_value = QString("");
+    if(path.isEmpty() || user_key.isEmpty()){
+        return false;
+    }
+    else{
+        //创建操作对象
+        QSettings * config = new QSettings(path,QSettings::IniFormat);
+
+        //读取
+        user_value = config->value(QString("config/") + user_key).toString();
+        return true;
+    }
+}
+
+bool LoginForm::rememberPass()
+{
+    if(ui->checkBox->isChecked()){
+        qDebug()<<"checked";
+        QString username = ui->lineEdit_un->text();  //账号
+        QString password = ui->lineEdit_pass->text();  //密码
+        qDebug()<<password;
+        bool is_address = writeInit(QString("./user.ini"), "username", username);
+        bool is_port = writeInit(QString("./user.ini"), "password", password);
+
+    }
+    else{
+        bool is_address = writeInit(QString("./user.ini"), "username", "");
+        bool is_port = writeInit(QString("./user.ini"), "password", "");
+
+        qDebug()<<"not checked";
+    }
+
 }
--- loginform.h
+++ loginform.h
@@ -14,6 +14,8 @@ class LoginForm :public QWidget
 public:
     explicit LoginForm(QWidget *parent = 0);
     ~LoginForm();
+    void load();
+    void save();
 
 protected:
 
@@ -28,6 +30,13 @@ private slots:
 
     void on_btnMenu_Close_clicked();
 
+    /**
+     *
+     * 记住密码
+     */
+    bool writeInit(QString path,QString user_key,QString user_value);
+    bool readInit(QString path,QString user_key,QString &user_value);
+    bool rememberPass();
 private:
     Ui::LoginForm *ui;
 };
