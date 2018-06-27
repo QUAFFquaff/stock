e8cb0888424fdafc1f4bcf5ff122920a9c17d695
QUAFFquaff <632633533@qq.com>
2018-06-27 09:58:56 +0800

    test 2018-6-27

--- loginform.cpp
+++ loginform.cpp
@@ -12,6 +12,7 @@ LoginForm::LoginForm(QWidget *parent) :
     ui(new Ui::LoginForm)
 {
     ui->setupUi(this);
+
     //this->setProperty("form", true);
     this->setProperty("canMove", true);
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowSystemMenuHint | Qt::WindowMinMaxButtonsHint);
@@ -19,6 +20,15 @@ LoginForm::LoginForm(QWidget *parent) :
     QUIWidget::setFormInCenter(this);
     IconHelper::Instance()->setIcon(ui->btnMenu_Min, QChar(0xF068));
     IconHelper::Instance()->setIcon(ui->btnMenu_Close, QChar(0xF00d));
+
+
+//    IconHelper::Instance()->setIcon(ui->labTitle, QChar(0xf0d1), 35);
+    IconHelper::Instance()->setIcon(ui->btnMenu_Min, QChar(0xF068));
+//    IconHelper::Instance()->setIcon(ui->btnMenu_Max, QChar(0xF067));
+    IconHelper::Instance()->setIcon(ui->btnMenu_Close, QChar(0xF00d));
+
+//    ui->label->setPixmap(QPixmap(QString::fromUtf8(":/tog.png")));
+
     //关联登录按钮点击
     connect(ui->pushButton_login,SIGNAL(clicked()),this,SLOT(doLoginButClick()));
 }
